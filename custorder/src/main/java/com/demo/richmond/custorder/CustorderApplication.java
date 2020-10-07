d37163605f10b1e72168f2bdaef7372aef10cc13
richmondchng <richmondchng@gmail.com>
2020-10-07 18:43:08 +0800

    initial commit

---
+++ custorder/src/main/java/com/demo/richmond/custorder/CustorderApplication.java
@@ -0,0 +1,13 @@
+package com.demo.richmond.custorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustorderApplication.class, args);
+	}
+
+}
---
+++ custorder/src/test/java/com/demo/richmond/custorder/CustorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.richmond.custorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
